5e1d54417ef75867503912719c7352ac056177ba
GunreddySrikanthreddy07 <gunreddysrikanthreddy07@gmail.com>
2023-05-31 14:55:35 +0530

    addition

---
+++ src/com/sathya/AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.sathya;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1=2;
+		int num2=5;
+		int result;
+		result=num1+num2;
+		System.out.println(result);
+
+	}
+
+}
